e9c760f1bf53aba2166fcc3e2e803e9922795f03
Kou-kun42 <shinigamithekido@gmail.com>
2020-10-23 18:18:32 -0400

    Finished

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ This is a doc string! It's a special kind of comment that is expected
 in Python files. Usually, you use this at the top of your code and in
 every function & class to explain what the code does.
 """
-from flask import Flask
+from flask import Flask, render_template
 
 # This is a comment, and it's also used to explain what your code is doing.
 # However, comments are more general, and can be used to denote different
@@ -21,27 +21,49 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+# Setting values to reference in the functions
+my_name = "Veer"
+
+halloween = "Saturday, October 31st"
+spooky_time = "5:00pm"
+
+guest_list = [{"name": "Jess", "rsvp": True},
+              {"name": "Meridith", "rsvp": True},
+              {"name": "Braus", "rsvp": False},
+              {"name": "Kami", "rsvp": True},
+              {"name": "Joi", "rsvp": False}]
+
+
+@app.route("/")
 def homepage():
     """Return template for home."""
     # TODO: Using Jinja2 templating, pass your name in to
     # The home page so that the page reads "Happy halloween <your name>!"
 
-    return render_template('index.html')
+    return render_template("index.html", name=my_name)
 
 # TODO: write a route for '/about'. Define a view function that renders a template 'about.html'
 # pass in the DAY and TIME of your halloween party as parameters, and render these dynamically in your templates.
+@app.route("/about")
+def aboutpage():
+    return render_template("about.html", day=halloween, time=spooky_time)
 
 
 # TODO: write a route for '/guests'. Create a list of "dummy" guests, and pass these into your 'guests.html' template.
 # HINT: create an <ul> tag  and use a Jinja for loop to render each "guest" in "guests" as an <li> element.
+@app.route("/guests")
+def guestspage():
+    return render_template("guests.html", guests=guest_list)
 
 
 # TODO: write a route for '/rsvp'. We aren't working on forms yet, but create a variable "rsvp" and set it to either True
 # or False. Then, in your 'rsvp.html' template, create an <h2> tag that shows IF a guest is rsvp'd, and another that will show
 # if they are NOT rsvp'd.
-
+@app.route("/rsvp")
+def rsvppage():
+    return render_template("rsvp.html", guests=guest_list)
 
 
 # TODO: Using your resources, try finishing this code so that you can run your app!
-if __name__ == ""
+if __name__ == "__main__":
+    app.run(debug=True)
