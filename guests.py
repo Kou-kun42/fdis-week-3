9f083b57f34105a4513a7d8f82f74409e387e4e1
Kou-kun42 <shinigamithekido@gmail.com>
2020-10-30 13:04:26 -0400

    Finished week 2 progress

---
+++ guests.py
@@ -0,0 +1,8 @@
+class Guest:
+    def __init__(self, name, email, plus_one, phone, costume, coming):
+        self.name = name
+        self.email = email
+        self.plus_one = plus_one
+        self.phone = phone
+        self.costume = costume
+        self.coming = coming
